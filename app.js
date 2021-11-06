0cf6e3a2fbd5d85fe3214740ac49900fb7ac72c6
Rae Alejandrino <raealejandrino@gmail.com>
2021-11-06 11:49:22 -0400

    Node module lesson 2 finish

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
 
-const printProfileData = profileDataArr => {
-    // This ...
+const [name, github] = profileDataArgs;
 
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
 
-    console.log('============');
+// const printProfileData = profileDataArr => {
+//     // This ...
+
+//     for (let i = 0; i < profileDataArr.length; i++) {
+//         console.log(profileDataArr[i]);
+//     }
+
+//     console.log('============');
      
-    // Is the same as this..
+//     // Is the same as this..
 
-    profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
+//     profileDataArr.forEach((profileItem) => console.log(profileItem));
+// };
 
-printProfileData(profileDataArgs);
+// printProfileData(profileDataArgs);
+
+
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!')
+});
+
