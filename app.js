c284ed466c3db9f8c24df8c7e7263fa9f0033da8
Rae Alejandrino <raealejandrino@gmail.com>
2021-11-08 10:46:27 -0500

    Implemented inquirer

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const pageHTML = generatePage(name, github);
 
-// const name = profileDataArgs[0];
-// const github = profileDataArgs[1];
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-const [name, github] = profileDataArgs;
+// // const name = profileDataArgs[0];
+// // const github = profileDataArgs[1];
+
+// const [name, github] = profileDataArgs;
 
 
 // const printProfileData = profileDataArr => {
@@ -27,9 +30,19 @@ const [name, github] = profileDataArgs;
 
 
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw err;
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
 
-    console.log('Portfolio complete! Check out index.html to see the output!')
-});
+//     console.log('Portfolio complete! Check out index.html to see the output!')
+// });
 
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
