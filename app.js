4424b5ff2d32f444656231780ae5c09b17371650
Rae Alejandrino <raealejandrino@gmail.com>
2021-11-08 11:34:24 -0500

    Added validation in prompts

--- app.js
+++ app.js
@@ -41,12 +41,28 @@ const promptUser = () => {
             {
                 type: 'input',
                 name: 'name',
-                message: 'What is your name?'
+                message: 'What is your name? (Required)',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter your name!');
+                        return false;
+                    }
+                }
             },
             {
                 type: 'input',
                 name: 'github',
-                message: 'Enter your GitHub Username'
+                message: 'Enter your GitHub Username',
+                validate: usernameInput => {
+                    if (usernameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter your GitHub Username!');
+                        return false;
+                    }
+                }
             },
             {
                 type: 'input',
@@ -74,12 +90,28 @@ Add a New Project
         {
             type: 'input',
             name: 'name',
-            message: 'What is the name of your project?'
+            message: 'What is the name of your project? (Required) ',
+            validate: projectNameInput => {
+                if (projectNameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project name!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'description',
-            message: 'Provide a description of the project (Required)'
+            message: 'Provide a description of the project (Required)',
+            validate: projectDescInput => {
+                if (projectDescInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project description!');
+                    return false;
+                }
+            }
         },
         {
             type: 'checkbox',
@@ -90,7 +122,15 @@ Add a New Project
         {
             type: 'input',
             name: 'link',
-            message: 'Enter the GitHub link to your project. (Required)'
+            message: 'Enter the GitHub link to your project. (Required)',
+            validate: githubLinkInput => {
+                if (githubLinkInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your GitHub link!');
+                    return false;
+                }
+            }
         },
         {
             type: 'confirm',
